e7f36cae85cf83d3f26daaa7148c0752f1bb03b2
Evan Moore <etmoore@gmail.com>
2014-10-29 10:25:37 -0600

    using company slogan for event description

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ Event.delete_all
 20.times do
   Event.create date: Faker::Time.forward(21),
               location: Faker::Address.city,
-              description: Faker::Lorem.sentence,
+              description: Faker::Company.catch_phrase,
               capacity: rand(501),
               requires_id: [true, false, false].sample
 end
